dae983a0a01d1215bd04834c27a2f2b71ab78558
arcwolf1 <16623722+arcwolf1@user.noreply.gitee.com>
2026-02-19 14:24:12 +0800

    6

--- src/config.ts
+++ src/config.ts
@@ -368,7 +368,7 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 export const commentConfig: CommentConfig = {
 	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
 	twikoo: {
-		envId: "https://twikoo.arcwolf.top",
+		envId: "https://twikoo.arcwolf.top/.netlify/functions/twikoo",
 		lang: SITE_LANG,
 	},
 };
